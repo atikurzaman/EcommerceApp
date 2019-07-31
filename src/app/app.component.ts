3660aed7eaa2b0c7dddcf01ad44713df75851e28
Kaptan <Kaptan.cse@gmail.com>
2019-08-01 03:01:08 +0600

    add bootswatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EcommerceAppSPA';
+export class AppComponent implements OnInit {  
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authSrvice:AuthService){}
+
+  ngOnInit(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.authSrvice.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
